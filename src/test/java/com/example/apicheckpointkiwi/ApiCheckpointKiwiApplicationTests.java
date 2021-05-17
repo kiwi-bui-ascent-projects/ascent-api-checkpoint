edd86e33f4ac9da435212c43cfbcfc23dbbf5482
kiwi-bui <kiwibui4@gmail.com>
2021-05-17 08:31:52 -0700

    Initialzr

---
+++ src/test/java/com/example/apicheckpointkiwi/ApiCheckpointKiwiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apicheckpointkiwi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCheckpointKiwiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
