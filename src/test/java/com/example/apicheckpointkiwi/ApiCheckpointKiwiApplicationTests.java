7c250cb20fb489bd14506b86fc4bbf2f9132465b
Kiwi Bui <kiwibui4@gmail.com>
2021-06-10 23:09:53 -0700

    added db properties

--- src/test/java/com/example/apicheckpointkiwi/ApiCheckpointKiwiApplicationTests.java
+++ src/test/java/com/example/apicheckpointkiwi/ApiCheckpointKiwiApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestPropertySource(locations= "classpath:application-test.properties")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApiCheckpointKiwiApplicationTests {
 	@Autowired
